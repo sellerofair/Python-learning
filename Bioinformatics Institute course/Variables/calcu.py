08c97461ea8e9ec8ebb6464527bc58cd42fffde6
Timur Akopyan <akopyan.t.i@inbox.ru>
2021-03-10 15:57:31 +0300

    square calculation

--- Bioinformatics Institute course/Variables/calcu.py
+++ Bioinformatics Institute course/Variables/calcu.py
@@ -1,24 +1,26 @@
 a, b, op = float(input()), float(input()), input()
 if op == '+':
     print(a + b)
-if op == '-':
+elif op == '-':
     print(a - b)
-if op == '*':
+elif op == '*':
     print(a * b)
-if op == '/':
+elif op == '/':
     if b == 0:
         print('Деление на 0!')
     else:
         print(a / b)
-if op == 'div':
+elif op == 'div':
     if b == 0:
         print('Деление на 0!')
     else:
         print(a // b)
-if op == 'mod':
+elif op == 'mod':
     if b == 0:
         print('Деление на 0!')
     else:
         print(a % b)
-if op == 'pow':
+elif op == 'pow':
     print(a ** b)
+else:
+    print('Недопустимая операция!')
